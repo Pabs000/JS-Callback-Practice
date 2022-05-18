6b58ffaef0f5a21bb1bf0068046e5d91f0210096
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-05-18 05:14:08 -0700

    new commit

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
 
 
+let direction = null;
+let x = 100;
+let y = 250;
+
+if(direction === 'west'){
+    x = x - 1
+}
+if(direction === 'north'){
+    y = y + 1 
+}
+
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
+    y = y - 1 
+}
+
+
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
