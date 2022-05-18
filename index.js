35b4632589986489a57dd81158c5b4179adf34b4
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-05-18 05:22:01 -0700

    movement

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
 
-
 let direction = null;
 let x = 100;
 let y = 250;
@@ -13,15 +12,17 @@ if(direction === 'west'){
     x = x - 1
 }
 if(direction === 'north'){
-    y = y + 1 
+    y = y + 1
 }
-
 if(direction === 'east'){
     x = x + 1
 }
 if(direction === 'south'){
-    y = y - 1 
+    y = y - 1
 }
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+setInterval(moveCharacter, 1)                                    
 
 
 move(newImage('assets/tree.png')).to(200, 450)
